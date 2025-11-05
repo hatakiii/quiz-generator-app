634b8c264100cf0890bd33d849678eacb4bd21ad
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-11-05 20:00:43 +0800

    summarized

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { ArticleQuizInput } from "@/app/_component/ArticleQuizInput";
+import { SummarizedContent } from "../_component/SummarizedContent";
 
 const page = () => {
   return (
     <div className="w-full h-full  flex  justify-center items-start ">
-      <ArticleQuizInput />
+      {/* <ArticleQuizInput /> */}
+      <SummarizedContent />
     </div>
   );
 };
--- src/app/_component/SummarizedContent.tsx
+++ src/app/_component/SummarizedContent.tsx
@@ -1,51 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import React from "react";
+import { FiFileText } from "react-icons/fi";
+import { LuSparkles } from "react-icons/lu";
 
-const SummarizedContent = () => {
+export const SummarizedContent = () => {
   return (
-    <div className="self-stretch p-7 bg-white rounded-lg outline  outline-border-border-border inline-flex flex-col justify-center items-start gap-5 overflow-hidden">
-      <div className="self-stretch inline-flex justify-start items-center gap-2">
-        <div className="w-8 h-8 relative overflow-hidden">
-          <div className="w-6 h-6  absolute outline  outline-border-border-foreground"></div>
+    <Card className="mt-12 h-fit p-7 bg-white rounded-lg outline  outline-border-border-border flex flex-col  gap-5 ">
+      <div className=" flex flex-col justify-start items-start gap-2">
+        <div className="self-stretch inline-flex justify-start items-start gap-2">
+          <LuSparkles className="w-8 h-8 " />
+
+          <div className="justify-start text-black text-2xl font-semibold  leading-8">
+            Article Quiz Generator
+          </div>
         </div>
-        <div className="justify-start text-black text-2xl font-semibold font-['Inter'] leading-8">
-          Article Quiz Generator
+        <div className="self-stretch justify-start text-zinc-500 text-base font-normal leading-5">
+          Paste your article below to generate a summarize and quiz question.
+          Your articles will saved in the sidebar for future reference.
         </div>
       </div>
-      <div className="self-stretch flex flex-col justify-start items-start gap-2">
-        <div className="self-stretch inline-flex justify-start items-center gap-2">
-          <div className="w-4 h-4 relative overflow-hidden">
-            <div className="w-3.5 h-3 left-[1.33px]  absolute outline outline-offset-[-0.50px] outline-border-border-foreground"></div>
-          </div>
-          <div className="justify-start text-neutral-500 text-sm font-semibold font-['Inter'] leading-5">
+      <div className="self-stretch flex flex-col justify-start items-start gap-1">
+        <div className="inline-flex justify-start items-center gap-1">
+          <FiFileText className="w-4 h-4" />
+          <div className="justify-start text-text-text-muted-foreground text-sm font-semibold leading-5">
             Summarized content
           </div>
         </div>
-        <div className="justify-start text-black text-2xl font-semibold font-['Inter'] leading-8">
-          Genghis khan
-        </div>
-        <div className="self-stretch flex flex-col justify-start items-start gap-1">
-          <div className="self-stretch justify-start text-black text-sm font-normal font-['Inter'] leading-5">
-            Genghis Khan, born Temüjin around 1162, was the founder of the
-            Mongol Empire. After his father's death, Temüjin's family was left
-            in poverty, and he later killed his half-brother to secure his
-            position. He built alliances with leaders like Jamukha and Toghrul,
-            and despite being defeated in battle and briefly under the Jin
-            dynasty, he rose to power by defeating rivals. By 1206, after
-            overcoming the Naiman tribe and executing Jamukha, Temüjin became
-            the undisputed ruler of the Mongol steppe, eventually leading a
-            series of successful military campaigns that expanded his empire
-            across China and Central Asia.
+        <div className="flex flex-col">
+          <div className="justify-start text-black text-2xl font-semibold font-['Inter'] leading-8">
+            Genghis khan
+          </div>
+          <div className="self-stretch flex flex-col justify-start items-start gap-1">
+            <div className="self-stretch justify-start text-black text-sm font-normal font-['Inter'] leading-5">
+              Genghis Khan, born Temüjin around 1162, was the founder of the
+              Mongol Empire. After his father's death, Temüjin's family was left
+              in poverty, and he later killed his half-brother to secure his
+              position. He built alliances with leaders like Jamukha and
+              Toghrul, and despite being defeated in battle and briefly under
+              the Jin dynasty, he rose to power by defeating rivals. By 1206,
+              after overcoming the Naiman tribe and executing Jamukha, Temüjin
+              became the undisputed ruler of the Mongol steppe, eventually
+              leading a series of successful military campaigns that expanded
+              his empire across China and Central Asia.
+            </div>
           </div>
         </div>
       </div>
-      <div className="self-stretch flex flex-col justify-end items-end gap-2">
-        <div className="self-stretch inline-flex justify-start items-center gap-1">
-          <div className="w-3.5 h-3.5 relative"></div>
-          <div className="justify-start text-text-text-muted-foreground text-sm font-semibold font-['Inter'] leading-5">
+      <div className="self-stretch flex flex-col justify-start items-start gap-1">
+        <div className="inline-flex justify-start items-center gap-1">
+          <FiFileText className="w-4 h-4" />
+          <div className="justify-start text-text-text-muted-foreground text-sm font-semibold  leading-5">
             Article Content
           </div>
         </div>
-        <div className="self-stretch flex flex-col justify-start items-start gap-1">
+        <div>
           <div className="self-stretch justify-start text-text-text-primary text-sm font-normal font-['Inter'] leading-5 line-clamp-3">
             Genghis Khan[a] (born Temüjin; c. 1162 – August 1227), also known as
             Chinggis Khan,[b] was the founder and first khan of the Mongol
@@ -73,19 +82,9 @@ const SummarizedContent = () => {
             as the sole ruler on the Mongolian steppe.
           </div>
         </div>
-        <div className="px-4 py-1 bg-background-bg-background rounded-md inline-flex justify-center items-center gap-2">
-          <div className="justify-start text-text-text-foreground text-sm font-medium font-['Inter'] leading-5">
-            See more
-          </div>
-        </div>
       </div>
-      <div className="h-10 px-6 py-2 bg-zinc-900 rounded-md inline-flex justify-center items-center gap-2">
-        <div className="justify-start text-text-text-primary-foreground text-sm font-medium font-['Inter'] leading-5">
-          Take a quiz
-        </div>
-      </div>
-    </div>
+
+      <Button variant="outline">Generate summary</Button>
+    </Card>
   );
 };
-
-export default SummarizedContent;
