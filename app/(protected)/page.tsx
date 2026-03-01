7eda66d8ef22fece760ae79ded3222cba5b81ad6
Khatanbaatar Ayushjav <25lp5598@nest.edu.mn>
2026-03-01 22:30:14 +0800

    Add console log to Home component

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,6 +1,7 @@
 import GenerateSummary from "../../components/main/GenerateSummary";
 
 export default function Home() {
+console.log("Hello")
   return (
     <div className=" w-full bg-secondary flex justify-center mt-14">
       <GenerateSummary />
